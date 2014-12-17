fa6ac97308374400fbaa507bc706c9b998edbb79
prston <a.kraft@foxel.ch>
2014-12-17 10:35:42 +0100

    Disable OSM Black&White tiles as the server has gone away

--- js/rawdata-navigator.js
+++ js/rawdata-navigator.js
@@ -927,12 +927,16 @@ var RawDataNavigator = new function() {
                     url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
                                     + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA</a>'
-                }, {
+                },
+                /*
+                {
                     description: 'OpenStreetMap Black and White',
                     url: 'http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png',
                     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
                                     + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA</a>'
-                }, {
+                },
+                */
+                {
                     description: 'Esri World Imagery',
                     url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
                     attribution: 'Tiles &copy; Esri, '
