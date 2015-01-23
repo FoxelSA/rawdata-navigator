ca339daa86112c8aaedadd0e502f99478c2a87e1
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2015-01-23 13:29:47 +0100

    unset viewport fixed scale

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=1920, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=1920, user-scalable=no">
     <title>DAV</title>
     <link rel="stylesheet" type="text/css" media="all" href="font-awesome-4.2.0/css/font-awesome.css" />
     <link rel="stylesheet" type="text/css" media="all" href="js/thirdparty/leaflet/leaflet.css" />
