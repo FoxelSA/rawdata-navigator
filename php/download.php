c6785b02d16b6e03baf5c104df84f5ae39a95120
prston <a.kraft@foxel.ch>
2015-01-28 11:59:09 +0100

    Download feature, has to be designed...

---
+++ php/download.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * rawdata-navigator - Human-understandable raw data navigator
+ *
+ * Copyright (c) 2014-2015 FOXEL SA - http://foxel.ch
+ * Please read <http://foxel.ch/license> for more information.
+ *
+ *
+ * Author(s):
+ *
+ *      Alexandre Kraft <a.kraft@foxel.ch>
+ *
+ *
+ * This file is part of the FOXEL project <http://foxel.ch>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://foxel.ch/license>.
+ */
+
+$ext = pathinfo($_GET['file'], PATHINFO_EXTENSION);
+if ($ext != 'jpeg' && $ext != 'ply')
+    return;
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename='.sprintf('"%s"',addcslashes(basename($_GET['file']),'"\\')));
+header('Content-Transfer-Encoding: binary');
+header('Connection: Keep-Alive');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Pragma: public');
+header('Content-Length: '.filesize($_GET['file']));
+
+echo file_get_contents($_GET['file']);
